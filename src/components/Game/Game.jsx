1a550748a2fe6b765027eeb0156554125f8c1f8a
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2022-01-11 12:23:01 +0200

    logics completed)))

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -9,12 +9,17 @@ export const Game = ({ namePlayerOne, namePlayerTwo }) => {
   const [winsPlayerTwo, setWinsPlayerTwo] = useState(0);
   const [preventWinner, setPreventWinner] = useState('playerOne');
 
-  // const draw = () => {
-  //   alert('Ничья');
-  //   setBoard(Array(9).fill(null));
-  // };
+  const win = (winner) => {
+    alert(`${winner} win`);
+    setBoard(Array(9).fill(''));
+  };
 
-  const checkWinner = (squares) => {
+  const draw = () => {
+    alert('Ничья');
+    setBoard(Array(9).fill(''));
+  };
+
+  const checkWinner = (boardCopy) => {
     let winnerLines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -26,54 +31,63 @@ export const Game = ({ namePlayerOne, namePlayerTwo }) => {
       [2, 4, 6]
     ];
 
-    // let isDraw = false;
+    let invalidLines = 0;
     
     for (let i = 0; i < winnerLines.length; i++) {
       const [a, b, c] = winnerLines[i];
+
+      if (
+        (boardCopy[a] === 'X'
+        || boardCopy[b] === 'X'
+        || boardCopy[c] === 'X')
+        && (boardCopy[a] === 'O'
+        || boardCopy[b] === 'O'
+        || boardCopy[c] === 'O')
+      ) {
+        invalidLines += 1;
+        
+        invalidLines === 8 && draw();
+      }
       
       if (
-        squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+        boardCopy[a] && (boardCopy[a] === boardCopy[b]) && (boardCopy[a] === boardCopy[c])
       ) {
-        if (preventWinner === 'playerOne' && squares[a] ==='X') {
+        if (preventWinner === 'playerOne' && boardCopy[a] ==='X') {
           setWinsPlayerOne(winsPlayerOne + 1)
           setPreventWinner('playerOne');
-        } else if (preventWinner === 'playerOne' && squares[a] ==='O') {
+          win(namePlayerOne);
+        } else if (preventWinner === 'playerOne' && boardCopy[a] ==='O') {
           setWinsPlayerTwo(winsPlayerTwo + 1)
           setPreventWinner('playerTwo');
-        } else if (preventWinner === 'playerTwo' && squares[a] ==='X') {
+          win(namePlayerTwo);
+        } else if (preventWinner === 'playerTwo' && boardCopy[a] ==='X') {
           setWinsPlayerTwo(winsPlayerTwo + 1)
           setPreventWinner('playerTwo');
-        } else if (preventWinner === 'playerTwo' && squares[a] ==='O') {
+          win(namePlayerTwo);
+        } else if (preventWinner === 'playerTwo' && boardCopy[a] ==='O') {
           setWinsPlayerTwo(winsPlayerOne + 1)
           setPreventWinner('playerOne');
+          win(namePlayerOne);
         }
 
-        setBoard(Array(9).fill(null));
-
-        return squares[a];
+        setBoard(Array(9).fill(''));
+        setXIsNext(true);
       }
-      // ни 1 из элементов в массивах если не содержит null, обнулить - вывести ничью
-      // squares[i].includes('')
-      // ? isDraw = false
-      // : isDraw = true;
-
     }
-
-    // isDraw && draw();
-    
-    return null;
   }
 
   const handlerClick = (index) => {
     const boardCopy = [...board];
-    
-    if (checkWinner(boardCopy) || boardCopy[index]) {
+
+    if (boardCopy[index]) {
       return;
     }
 
     boardCopy[index] = xIsNext ? 'X' : 'O';
-    setXIsNext(!xIsNext);
+
     setBoard(boardCopy);
+    setXIsNext(!xIsNext);
+    checkWinner(boardCopy);
   };
 
   return (
